81e52dd63b479f714ce69491221f024db6a76e04
avayyyyyyy <jabhi465@gmail.com>
2024-05-27 11:53:51 +0530

    perf: ✅ Added target in links

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -9,6 +9,7 @@ function Footer() {
         <div className="mx-auto max-w-screen-xl px-4 py-5 sm:px-6 md:px-8">
           <div className="sm:flex sm:items-center sm:justify-between">
             <Link
+              target="_blank"
               href={"https://shubhcodes.vercel.app"}
               className="flex justify-center items-center bg-gradient-to-br from-gray-100 to-gray-400 h-fit font-semibold text-lg  text-transparent bg-clip-text sm:justify-start"
             >
